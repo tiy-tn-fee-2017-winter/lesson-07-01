c3199a22d41d2bcc190e2151c8fdb6ebd938b435
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-13 11:59:11 -0500

    Ask for username

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Load Config
 // import config from './knexfile';
 const config = require('./knexfile');
+const inquirer = require('inquirer');
 
 // Load knex library
 const KnexLib = require('knex');
@@ -9,6 +10,30 @@ const database = KnexLib(config.development);
 console.log('ITS WORKING');
 
 async function app() {
+  const answers = await inquirer.prompt([{
+    type: 'input',
+    name: 'username',
+    message: 'Who are you?',
+  }]);
+
+  // SELECT * FROM users WHERE username = ? LIMIT 1
+  const user = await database.select()
+    .from('users')
+    .where({ username: answers.username })
+    .limit(1)
+    // KNEX look for first in result
+    .first();
+
+  if (user === undefined) {
+    const newUser = await database
+      .insert({ id: 8, username: answers.username })
+      .into('users');
+
+    console.log('NEWUSER', newUser);
+  } else {
+    console.log('USER', user);
+  }
+
   // Select from "users" table
   const users = await database.select().from('users');
 
