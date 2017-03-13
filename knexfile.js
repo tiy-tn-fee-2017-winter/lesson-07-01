80cabaa64a225122fc4c393da845adf4d55b9194
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-13 12:08:53 -0500

    Setup .env for configuration

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,13 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'todoapp',
-      user:     'ryan',
-      password: ''
+      database: process.env.DB_DATABASE,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       tableName: 'knex_migrations'
